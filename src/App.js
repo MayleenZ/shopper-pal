9592d5f24ef97d028e5296fc98c8f4a54cbd31ae
Mayleen <mayleennyc@gmail.com>
2023-04-28 13:31:14 -0400

    pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import {useState} from 'react';
 
 import { Routes, Route } from 'react-router-dom';
-
-import NewOrderPage from './pages/NewOrderPage';
 import AuthPage from './pages/AuthPage';
-import OrderHistoryPage from './pages/OrderHistoryPage';
 import NavBar from './components/NavBar';
+
+import CustomPage from './pages/CustomPage'
+import HomePage from './pages/HomePage'
+import ReadyTemplatePage from './pages/ReadyTemplatePage'
+import SavedPage from './pages/SavedPage';
 
 import { getUser } from './utilities/users-service';
 
 import './App.css';
+
 
 function App() {
   const [user, setUser] = useState(getUser());
@@ -20,8 +23,10 @@ function App() {
       <>
       <NavBar user={user} setUser={setUser}/>
       <Routes>
-        <Route path='/orders/new' element={ <NewOrderPage /> }/>
-        <Route path='/orders' element={ <OrderHistoryPage /> }/>
+        <Route path='/custom' element={ <CustomPage /> }/>
+        <Route path='/home' element={ <HomePage /> }/>
+        <Route path = '/saved'element= {<SavedPage/>}/>
+        <Route path = '/pre-made' element={<ReadyTemplatePage/>} />
       </Routes>
       </>
      : 
